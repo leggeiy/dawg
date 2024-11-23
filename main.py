32cb99e385f53841fe9b8165772ce72e7206d136
zayn :D <156433676+pharwent@users.noreply.github.com>
2024-11-24 06:35:34 +0700

    Create main.py
    NOT MY CODE, i did add some math to it but anything other than that is NOT MINE

---
+++ main.py
@@ -0,0 +1,106 @@
+
+import discord
+import random
+import os
+from discord.ext import commands
+from bot_logic import gen_pass
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+# command prefix 
+bot = commands.Bot(command_prefix='$', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})') # type: ignore
+    print('------')
+
+# adding two numbers
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def subtract(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left - right)
+
+@bot.command()
+async def div(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left / right)
+
+@bot.command()
+async def multiply(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left * right)
+
+@bot.command()
+async def by(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left ** right)
+
+# spamming word
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+        
+# password generator        
+@bot.command()
+async def pw(ctx):
+    await ctx.send(f'Kata sandi yang dihasilkan: {gen_pass(10)}')
+
+# coinflip
+@bot.command()
+async def coinflip(ctx):
+    num = random.randint(1,2)
+    if num == 1:
+        await ctx.send('It is Head!')
+    if num == 2:
+        await ctx.send('It is Tail!')
+
+# rolling dice
+@bot.command()
+async def dice(ctx):
+    nums = random.randint(1,6)
+    if nums == 1:
+        await ctx.send('It is 1!')
+    elif nums == 2:
+        await ctx.send('It is 2!')
+    elif nums == 3:
+        await ctx.send('It is 3!')
+    elif nums == 4:
+        await ctx.send('It is 4!')
+    elif nums == 5:
+        await ctx.send('It is 5!')
+    elif nums == 6:
+        await ctx.send('It is 6!')
+
+# @bot.command()
+# async def mem(ctx):
+#     # try by your self 2 min
+#     img_name = random.choice(os.listdir('images'))
+#     with open(f'images/{img_name}', 'rb') as f:
+#         picture = discord.File(f)
+ 
+#     await ctx.send(file=picture)
+
+# welcome message
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}') # type: ignore
+    # provide what you can help here
+
+
+bot.run('haha you thought')
